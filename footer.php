abbb1eb0e3d20624f95219592e01bf0a49cc0b69
websmith <n8cornelius@gmail.com>
2021-11-03 13:24:48 -0500

    remove newsletter subscription from footer

--- footer.php
+++ footer.php
@@ -1,23 +1,17 @@
 <!-- Footer -->
-<!-- <section id="cta" class="wrapper style4">
-	<div class="inner">
-		<header>
-			<h2>Subscribe to our newsletter!</h2>
-			<p>Subscribe to receive updates on our journeys, writings, and resources!</p>
-		</header>
-		<?php echo do_shortcode('[mc4wp_form id="935"]'); ?>
-	</div>
-</section> -->
 
 <footer id="footer">
-	<ul class="icons">
-		<li><a href="https://www.instagram.com/kurtfrederickmahler/" target="_blank" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-		<li><a href="https://www.facebook.com/kurtfrederick.mahler" target="_blank" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-	</ul>
-	<ul class="copyright">
-		<li>&copy; <?php echo date('Y')." ".get_bloginfo('name'); ?></li><li>Website by: <a href="https://apparatusagency.com/" target="_blank">Apparatus</a></li>
-	</ul>
-	<p class="attribution">Web Design attributed to: <a href="https://html5up.net/" target="_blank">HTML5 UP</a></p>
+  <ul class="icons">
+    <li><a href="https://www.instagram.com/kurtfrederickmahler/" target="_blank" class="icon brands fa-instagram"><span
+          class="label">Instagram</span></a></li>
+    <li><a href="https://www.facebook.com/kurtfrederick.mahler" target="_blank" class="icon brands fa-facebook-f"><span
+          class="label">Facebook</span></a></li>
+  </ul>
+  <ul class="copyright">
+    <li>&copy; <?php echo date('Y')." ".get_bloginfo('name'); ?></li>
+    <li>Website by: <a href="https://apparatusagency.com/" target="_blank">Apparatus</a></li>
+  </ul>
+  <p class="attribution">Web Design attributed to: <a href="https://html5up.net/" target="_blank">HTML5 UP</a></p>
 </footer>
 
 </div><!-- page-wrapper -->
@@ -25,4 +19,5 @@
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
